aef87c0795d19ca34a233f7f6796ed86e363aae1
Gidraf Orenja <gidraforenja@outlook.com>
2022-09-29 17:47:57 +0300

    Add components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 
 const Home = ()=>{
 
     return (
-        <View>
-            <Text>Welcome home</Text>
+        <View style={styles.container}>
+            <Text style={styles.toolbar}>Best Of ChuckNorris</Text>
         </View>
     )
 }
 export default Home
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor:"blue",
+      padding:10,
+    },
+    toolbar:{
+   
+    }
+  });
